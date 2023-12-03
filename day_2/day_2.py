a9e32afaaf9e45227712809c31f8b7f80647738a
celian <celian.lombardot@epitech.eu>
2023-12-03 19:24:58 +0100

    Improve of a couple bytes

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -2,15 +2,14 @@
 e={'r':0,'g':1,'b':2}
 t,p,i=0,0,1
 for l in open("input.txt", "r").read().split('\n'):
- a,r=[0,0,0],True
+ a,r=[0]*3,1
  for c in l.split(':')[1].replace(',',';').split(';'):
   j=0
-  while not c[j]in'rgb':j+=1
+  while(c[j]in'rgb')<1:j+=1
   n,j=int(c[:j]),e[c[j]]
   if n>a[j]:a[j]=n
-  if n>12+j:r=False
+  if n>12+j:r=0
  if r:t+=i
  i+=1
  p+=a[0]*a[1]*a[2]
-print("part 1:",t)
-print("part 2:",p)
+print("part 1:",t,"\npart 2:",p)
