7b2a508bedb8fcd99d72ddbfa637ed8689473ebd
Célian Lombardot <celian.lombardot@epitech.eu>
2023-12-04 16:54:59 +0100

    Fix wrong day

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def s(l,a,j,t):
- for i,b in enumerate('one,two,three,four,five,six,seven,eight,nine'.split(',')):
-  if t and l[j:][:len(b)]==b:return-~i
- return int(l[j].isdigit()and l[j]or s(l,a,j+a,t))
-def r(a,t=0):
- for l in open("input","r").readlines():t+= s(l,1,0,a)*10+s(l,-1,-1,a)
- return t
-print("part 1:",r(0),"part 2:",r(1))
+f=open("input.txt","r").readlines()
+t=i=0
+d=[1]*len(f)
+for e in f:
+ c=38-len(set(e[e.index(':'):-1].split(' ')))
+ t+=2**(c-1)//1
+ while c:d[i+c]+=d[i];c-=1
+ i+=1
+print("part 1:",t,"\npart 2:",sum(d))
