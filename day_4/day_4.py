909d5ac71b14c2676bd21cbc6e742be1baa81cbe
Célian Lombardot <celian.lombardot@epitech.eu>
2023-12-04 16:47:46 +0100

    Update day_4, probably minimum

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,31 +1,9 @@
 #!/usr/bin/python3
-
-f = open("input.txt", "r").read().split('\n')
-
-def nbs(l):
-    return [int(v) for v in l.split(' ') if v!='']
-
-def value(card):
-    w,y=card.split('|')
-    w,y=nbs(w),nbs(y)
-
-    t=0
-    n=0
-    for v in w:
-        if v in y:
-            n+=1
-            if t==0:t=1
-            else: t*=2
-    return t,n
-
-t=0
-d=[1]*len(f)
-for i in range(len(f)):
-    card=f[i].split(':')[1]
-    z,c=value(card)
-    t+=z
-    for j in range(c):
-        d[i+1+j]+=d[i]
-print("part 1:",t)
-print("part 2:",sum(d))
-
+def s(l,a,j,t):
+ for i,b in enumerate('one,two,three,four,five,six,seven,eight,nine'.split(',')):
+  if t and l[j:][:len(b)]==b:return-~i
+ return int(l[j].isdigit()and l[j]or s(l,a,j+a,t))
+def r(a,t=0):
+ for l in open("input","r").readlines():t+= s(l,1,0,a)*10+s(l,-1,-1,a)
+ return t
+print("part 1:",r(0),"part 2:",r(1))
