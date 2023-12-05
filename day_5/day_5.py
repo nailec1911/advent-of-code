463ba9ea0a9130f3135ea942eb0ac141c0d5d6a1
Célian Lombardot <celian.lombardot@epitech.eu>
2023-12-05 13:48:16 +0100

    Update  final day_5

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,72 +1,20 @@
 #!/usr/bin/python3
-
-f = open("input.txt", "r").read().split('\n')
-
-# f= ["seeds: 79 14 55 13",
-#     "",
-#     "seed-to-soil map:",
-#     "50 98 2",
-#     "52 50 48",
-#     "",
-#     "soil-to-fertilizer map:",
-#     "0 15 37",
-#     "37 52 2",
-#     "39 0 15",
-#     "",
-#     "fertilizer-to-water map:",
-#     "49 53 8",
-#     "0 11 42",
-#     "42 0 7",
-#     "57 7 4",
-#     "",
-#     "water-to-light map:",
-#     "88 18 7",
-#     "18 25 70",
-#     "",
-#     "light-to-temperature map:",
-#     "45 77 23",
-#     "81 45 19",
-#     "68 64 13",
-#     "",
-#     "temperature-to-humidity map:",
-#     "0 69 1",
-#     "1 0 69",
-#     "",
-#     "humidity-to-location map:",
-#     "60 56 37",
-#     "56 93 4"]
-
-def get_list(f):
-    f.pop(0)
-    r=[]
-    while f and f[0]!="":
-        r.append([int(e) for e in f.pop(0).split(' ')])
-    if f:f.pop(0)
-    return r
-
-def convert(seeds, conv):
-    for i in range(len(seeds)):
-        s=seeds.pop(0)
-        v=0
-        for c in conv:
-            sta,end=max(s[0], c[1]),min(s[1], c[1]+c[2])
-            if sta<end:
-                v=1
-                if s[0]<sta:seeds+=[s[0],sta],
-                seeds+=[sta-c[1]+c[0],end-c[1]+c[0]],
-                if end<s[1]:seeds+=[end,s[1]],
-                break
-        if v<1:seeds+=s,
-
-seeds = [int(e) for e in f.pop(0)[7:].split(' ')]
-pairs= [[seeds[i],seeds[i]+seeds[i+1]] for i in range(0,len(seeds),2)]
-seeds=[[seeds[i],seeds[i]+1] for i in range(len(seeds))]
-f.pop(0)
-
+def t(l,c):
+ for z in l[::]:
+  a,b=l.pop(0);r=[a,b],
+  for e,f,g in c:
+   y,z=max(a,f),min(b,f+g)
+   if y<z:
+    r=[y-f+e,z-f+e],
+    if a<y:r+=[a,y],
+    if z<b:r+=[z,b],
+    break
+  l+=r
+f=open("input.txt","r").read().split('\n')
+s=[[int(e),int(e)+1]for e in f.pop(0)[7:].split(' ')]
+l=[[a[0],a[0]+b[0]]for a,b in zip(s,s[1:])][::2]
 while f:
-    conv=get_list(f)
-    convert(seeds, conv)
-    convert(pairs,conv)
-
-print("part 1:",min(seeds)[0])
-print("part 2:",min([e[0] for e in pairs]))
+ f.pop(0)*2;f.pop(0);c=[]
+ while f and f[0]!="":c+=[int(e) for e in f.pop(0).split(' ')],
+ t(s,c);t(l,c)
+print("part 1:",min(s)[0],"\npart 2:",min(l)[0])
