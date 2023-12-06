4b4076838f5ad9ef024f22686042df05bc13ffa3
Célian Lombardot <celian.lombardot@epitech.eu>
2023-12-06 11:14:58 +0100

    Update smaller day_6

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
-f=open("input.txt","r").read().split('\n')
-a,b=[],[]
-for i in 0,1:
-    f[i]=f[i].split(':')[1].split(' ')
-    a+=[int(e)for e in f[i]if e],
-    b+=int(''.join(f[i])),
-
-def dist(t,d):return d*(t-d)
-
-def first(time,r):
-    for i in range(time):
-        if dist(time,i)>r:
-            return i
-    return 0
-
-def last(time,r):
-    for i in range(time,-1,-1):
-        if dist(time,i)>r:
-            return i
-    return 0
-
+def v(t,d,o):
+ i=o*t
+ while 0<=i<=t:
+  if i*(t-i)>d:return i
+  i+=-o*2+1
+ return 0
+w=lambda t:v(*t,1)-v(*t,0)+1
+f=[[int(x) for x in e.split(':')[1].split(' ')if x]for e in open("input.txt","r").read().split('\n')]
 s=1
-for i in range(len(a[0])):
-    s*=last(a[0][i],a[1][i])-first(a[0][i],a[1][i])+1
-print("part 1:",s,"\npart 2:",last(b[0],b[1])-first(b[0],b[1])+1)
+for e in[z for z in zip(*f)]:s*=w(e)
+print("part 1:",s,"\npart 2:",w([int(''.join(map(str,e)))for e in f]))
