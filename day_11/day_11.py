d843052b88ca8aee3d593eb52d1abc5892230692
Célian Lombardot <celian.lombardot@epitech.eu>
2023-12-11 15:32:21 +0100

    Update final day_11

--- day_11/day_11.py
+++ day_11/day_11.py
@@ -1,66 +1,24 @@
 #!/usr/bin/python3
-f=list(open('input.txt'))
-
-# f=[
-# "...#......\n",
-# ".......#..\n",
-# "#.........\n",
-# "..........\n",
-# "......#...\n",
-# ".#........\n",
-# ".........#\n",
-# "..........\n",
-# ".......#..\n",
-# "#...#.....\n"]
-f=[list(e[:-1])for e in f]
-
-def empty_line(f,i): return not '#' in f[i]
-def empty_col(f,j): return not '#' in [l[j]for l in f]
-
-large=[e[::]for e in f]
-large.append([i for i in range(len(f[0]))])
-for i in range(len(large)):
-    large[i].insert(0,i)
-
-f=[e[::]for e in large]
-
-
-i=0
-while i<len(f):
-    if empty_line(f,i):
-        for j in range(i,len(f)):
-            f[j][0]+=1
-            large[j][0]+=999999
-        i+=1
-    i+=1
-i=1
-while i<len(f[0]):
-    if empty_col(f,i):
-        for j in range(i,len(f[0])):
-            f[-1][j]+=1
-            large[-1][j]+=999999
-
-    i+=1
-
-
-def dist(i,j,k,l):
-    return  k-i+abs(l-j)
-
-def check_all(f,i,j):
-    r=0
-    for k in range(i,len(f)):
-        for l in range(len(f[0])):
-            if f[k][l]=='#':
-                r+=dist(f[i][0],f[-1][j],f[k][0],f[-1][l])
-    return r
-
-t=0
-t2=0
-for i in range(len(f)):
-    for j in range(len(f[0])):
-        if f[i][j]=='#':
-            f[i][j]='.'
-            large[i][j]='.'
-            t+=check_all(f,i,j)
-            t2+=check_all(large,i,j)
-print(t,t2)
+f=[list(e[:-1])for e in list(open('input.txt'))]
+m,l=len(f),len(f[0])
+def v(c,r):
+ t=0
+ for k in range(m*l):
+  if'#'==f[k//l][k%l]:t+=r[k//l]-r[i]+abs(c[k%l]-c[j])
+ return t
+r=[]
+c=[]
+rl=[]
+cl=[]
+q=u=0
+for i in range(m):
+ if{*f[i]}=={'.'}:q+=1
+ r+=i+q*1,;rl+=i+q*999999,
+for i in range(l):
+ if{e[i]for e in f}=={'.'}:u+=1
+ c+=i+u*1,;cl+=i+u*999999,
+x=y=0
+for i in range(m*l):
+ i,j=i//l,i%l
+ if'#'==f[i][j]:f[i][j]='.';x+=v(c,r);y+=v(cl,rl)
+print("part 1:",x,"\npart 2:",y)
