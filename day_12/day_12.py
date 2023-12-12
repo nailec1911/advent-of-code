6397b162a1809372618524ba6cfdad733d956fd2
Célian Lombardot <celian.lombardot@epitech.eu>
2023-12-12 12:59:08 +0100

    Update final day_12 (0.25s slower, but less than 500 char)

--- day_12/day_12.py
+++ day_12/day_12.py
@@ -1,44 +1,19 @@
 #!/usr/bin/python3
-f=list(open('input.txt'))
-
-# f=[
-# "???.### 1,1,3\n",
-# ".??..??...?##. 1,1,3\n",
-# "?#?#?#?#?#?#?#? 1,3,1,6\n",
-# "????.#...#... 4,1,1\n",
-# "????.######..#####. 1,6,5\n",
-# "?###???????? 3,2,1\n"]
-
-def possibilities(l,n,li,ni):
-    k=(li,ni)
-    if k in MEM: return MEM[k]
-    r=0
-    if ni>=len(n):
-        if '#'in l[li:]: return 0
-        return 1
-    if li==len(l):return 0
-    i=0
-    while l[li+i]in '.?#':
-        d=0
-        while l[li+i+d]in'?#':d+=1
-        if d>=n[ni] and l[li+i+n[ni]]!='#':
-            r+=possibilities(l,n,li+i+n[ni]+1,ni+1)
-        if l[li+i]=='#':break
-        if len(l[li:])-1<sum(n[ni:])+len(n[ni:]):break
-        i+=1
-    MEM[k]=r
-    return r
-
-t1=t2=0
-for i in range(len(f)):
-    l,n=f[i].split(' ')
-    n=[int(e) for e in n.split(',')]
-    MEM={}
-    t1+=possibilities(l+' ',n,0,0)
-    MEM={}
-    t2+=possibilities(((l+'?')*5)[:-1]+' ',n*5,0,0)
-
-print(t1,t2)
-
-
-# print("part 1:",x,"\npart 2:",y)
+def s(l,n,p,x):
+ k=(p,x);r=i=0
+ if k in M:return M[k]
+ if x>=len(n):
+  if'#'in l[p:]:return 0
+  return 1
+ if p==len(l):return 0
+ while l[p+i]in'.?#':
+  d=0
+  while l[p+i+d]in'?#':d+=1
+  if d>=n[x]and'#'!=l[p+i+n[x]]:r+=s(l,n,p+i+n[x]+1,x+1)
+  if'#'==l[p+i]:break
+  i+=1
+ M[k]=r
+ return r
+a=b=0
+for l in list(open('input.txt')):l,n=l.split(' ');n=[int(e) for e in n.split(',')];M={};a+=s(l+' ',n,0,0);M={};b+=s(((l+'?')*5)[:-1]+' ',n*5,0,0)
+print("part 1:",a,"\npart 2:",b)
