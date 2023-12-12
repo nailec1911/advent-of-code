e523699ddede555048c07bb3ae111f96c9637d2c
Célian Lombardot <celian.lombardot@epitech.eu>
2023-12-12 11:48:22 +0100

    Add day 12 working (but use of cache so need to be improved)

---
+++ day_12/day_12.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+f=list(open('input.txt'))
+
+# f=[
+# "???.### 1,1,3\n",
+# ".??..??...?##. 1,1,3\n",
+# "?#?#?#?#?#?#?#? 1,3,1,6\n",
+# "????.#...#... 4,1,1\n",
+# "????.######..#####. 1,6,5\n",
+# "?###???????? 3,2,1\n"]
+
+
+from functools import cache
+
+@cache
+def possibilities(l,n,li,ni):
+    r=0
+    if ni>=len(n):
+        if '#'in l[li:]: return 0
+        return 1
+    if li==len(l):return 0
+    i=0
+    while l[li+i]in '.?#':
+        d=0
+        while l[li+i+d]in'?#':d+=1
+        if d>=n[ni] and l[li+i+n[ni]]!='#':
+            r+=possibilities(l,n,li+i+n[ni]+1,ni+1)
+        if l[li+i]=='#':break
+        if len(l[li:])-1<sum(n[ni:])+len(n[ni:]):break
+        i+=1
+
+    return r
+
+
+
+t1=t2=0
+for i in range(len(f)):
+    # print(i)
+    l,n=f[i].split(' ')
+    n=[int(e) for e in n.split(',')]
+    t1+=possibilities(tuple(l+' '),tuple(n),0,0)
+    t2+=possibilities(tuple(((l+'?')*5)[:-1]+' '),tuple(n*5),0,0)
+    # print(t2)
+
+print(t1,t2)
+
+
+
+# print("part 1:",x,"\npart 2:",y)
