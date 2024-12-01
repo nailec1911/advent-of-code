74b5609d960455a37b41254b9f14f2aa254bb545
celian <celian.lombardot@epitech.eu>
2024-12-01 06:55:06 +0100

    improve(1): make the day cleaner

--- 2024/day_01/day_01.py
+++ 2024/day_01/day_01.py
@@ -1,48 +1,26 @@
-a = ["3   4\n",
-"4   3\n",
-"2   5\n",
-"1   3\n",
-"3   9\n",
-"3   3\n" ]
+from collections import Counter
 
+# example = ["3   4\n",
+# "4   3\n",
+# "2   5\n",
+# "1   3\n",
+# "3   9\n",
+# "3   3\n" ]
+# f=[e[:-1] for e in list(example)]
 
-
-f=[e[:-1] for e in list(open("input.txt"))]
-# f=[e[:-1] for e in list(a)]
-
-
-f = [e.split() for e in f]
+f = [e[:-1] for e in list(open("input.txt"))]
 
 
 right = []
 left = []
-
 for e in f:
-    left.append(int(e[0]))
-    right.append(int(e[1]))
-
-
-
-tot = 0
+    l, r = e.split()
+    left.append(int(l))
+    right.append(int(r))
 
 right.sort()
 left.sort()
+sim = Counter(right)
 
-sim = {}
-
-for i in range(len(right)):
-    if right[i] in sim.keys():
-        sim[right[i]] += 1
-    else:
-        sim[right[i]] = 1
-    a = abs(right[i] - left[i])
-    tot += a
-
-print("part1", tot)
-
-tot = 0
-for e in left:
-    if e in sim.keys():
-        tot += e * sim[e]
-
-print(tot)
+print(sum(abs(a - b)for a, b in zip(left, right)))
+print(sum(e * sim[e] for e in left))
