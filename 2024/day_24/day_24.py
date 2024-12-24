7f1bd7cffc1e60e0ee88b44421a914660a22fa43
celian <celian.lombardot@epitech.eu>
2024-12-24 15:25:39 +0100

    fix(2024-24): do part 2 automatically

--- 2024/day_24/day_24.py
+++ 2024/day_24/day_24.py
@@ -1,11 +1,13 @@
-f = [e[:-1] for e in list(open("input.txt"))]
+from itertools import combinations
+
+f = [e[:-1] for e in list(open("input.txt", encoding='utf-8'))]
 separator = f.index('')
 
 start = {}
 for line in f[:separator]:
-    wire, val = line.split(' ')
-    val = int(val)
-    start[wire[:-1]] = val
+    wire, val_bin = line.split(' ')
+    val_bin = int(val_bin)
+    start[wire[:-1]] = val_bin
 
 SIZE = len(start) // 2
 
@@ -26,90 +28,67 @@ while any([not a[-1] for a in operations]):
         start[wire3] = res
         operations[i][-1] = True
 
-l = []
-for key, val in start.items():
-    if not key.startswith('z'):
-        continue
-    l.append((key, val))
-l.sort()
-
-score: int = 0
-for z, val in reversed(l):
-    score <<= 1
-    score |= val
-
-print(score)
+l = sorted([elt for elt in start.items() if elt[0][0] == 'z'])
+l = [str(e[1]) for e in l[::-1]]
+print(int(''.join(l), 2))
 
 
-def search(links, w1, keyword, w2, w3):
-    for i, e in enumerate(links):
-        if (e[0] == w1 and e[1] == keyword and e[2] == w2) or (e[0] == w2 and e[1] == keyword and e[2] == w1):
-            if e[3] == w3:
-                return True, i
-            return False, i
-    return False, -1
+def check_op(links, w1, keyword, w2):
+    for e in links:
+        if (e[0] == w1 and e[2] == w2 and e[1] == keyword) or (e[0] == w2 and e[2] == w1 and e[1] == keyword):
+            return e[3]
+    return None
 
 
-def check_op(links, w1, keyword, w2, w3=""):
-    for i, e in enumerate(links):
-        if (e[0] == w1 and e[1] == keyword and e[2] == w2) or (e[0] == w2 and e[1] == keyword and e[2] == w1):
-            if w3 == "":
-                return True, i
-            if e[3] == w3:
-                return True, i
-            return False, i
-    return False, -1
+def find_index_error(idx, ok):
+    for i in range(idx, SIZE):
+        x = f"x{i:02}"
+        y = f"y{i:02}"
+
+        val = check_op(operations, x, "XOR", y)
+        temp_carry = check_op(operations, x, "AND", y)
+        if i == 0:
+            carries[0] = temp_carry
+            continue
+        carry = carries[i - 1]
+
+        temp = check_op(operations, carry, "AND", val)
+        resz = check_op(operations, carry, "XOR", val)
+        if resz != f"z{i:02}":
+            return i, ok, [temp_carry, val, temp, resz, carry]
+
+        ok.add(temp_carry)
+        ok.add(val)
+        ok.add(temp)
+        ok.add(resz)
+        carries[i] = check_op(operations, temp, "OR", temp_carry)
+        ok.add(carry)
+
+    return 44, ok, [temp_carry, val, temp, resz, carry]
 
 
-def find_res(links, w3):
-    for i, e in enumerate(links):
-        if e[3] == w3:
-            return i
-    return 0
+carries = {}
+swaps = []
+used = set()
+to_change, used, wrongs = find_index_error(0, used)
 
+for _ in range(4):
+    for i, j in combinations(range(len(operations)), 2):
+        res_i = operations[i][3]
+        res_j = operations[j][3]
 
-check_op(operations, "x00", "XOR", "y00", "z00")
-ok, idx = check_op(operations, "x00", "AND", "y00")
-carry = operations[idx][3]
-before = idx
-not_ok = set()
+        if not (res_i in wrongs or res_j in wrongs):
+            continue
+        if res_i in used or res_j in used:
+            continue
+        operations[i][3] = res_j
+        operations[j][3] = res_i
+        new, new_used, new_wrongs = find_index_error(to_change, used)
+        if new > to_change:
+            swaps += [res_i, res_j]
+            to_change, used, wrongs = new, new_used, new_wrongs
+            break
+        operations[i][3] = res_i
+        operations[j][3] = res_j
 
-for i in range(1, SIZE):
-    x = f"x{i:02}"
-    y = f"y{i:02}"
-    z = f"z{i:02}"
-    print(x, y, z)
-
-    # carry : rfd
-    ok, idx = check_op(operations, x, "XOR", y)
-    val = operations[idx][3]  # smf
-    if not ok:
-        not_ok.add(i)
-
-    ok, idx = check_op(operations, x, "AND", y)
-    temp_carry = operations[idx][3]  # wkw
-    if not ok:
-        not_ok.add(i)
-
-    ok, idx = check_op(operations, carry, "AND", val)
-    temp = operations[idx][3]  # jgr
-    if not ok:
-        not_ok.add(i)
-
-    ok, idx = check_op(operations, carry, "XOR", val, z)  # fcd
-    if not ok:
-        not_ok.add(i)
-
-    ok, idx = check_op(operations, temp, "OR", temp_carry)
-    carry = operations[idx][3]
-    before = idx
-    if not ok:
-        not_ok.add(i)
-    if i in not_ok:
-        print("ooooooooooooo")
-
-
-# z16,hmk
-# z20,fhp
-# tpc,rvf
-# z33,fcd
+print(','.join(sorted(swaps)))
